6e93de2942793022252dbaf4c319d66941fc5220
Stephanie <stephaniereyes@pursuit.org>
2022-02-18 13:38:31 -0500

    changes to schema

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,9 +3,14 @@ CREATE DATABASE cta_dev;
 
 \c cta_dev;
 
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS candles;
 
-CREATE TABLE test (
+CREATE TABLE candles (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT,
+    description TEXT,
+    price INTEGER DEFAULT 0,
+    rating INTEGER DEFAULT 0,
+    featured BOOLEAN,
+    image TEXT
 );
