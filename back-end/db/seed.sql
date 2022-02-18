546c19ee37db4e493a5775fecf178d9eccd9a9ab
Stephanie <stephaniereyes@pursuit.org>
2022-02-18 14:23:15 -0500

    changes to seed

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,10 @@
 \c cta_dev;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO snacks (name,description,price,rating,feautured,image ) VALUES
+('fenwick Candles','Organic Coconut wax candle',18.99,4.5,true,'https://images.unsplash.com/photo-1588372405219-e40d64efafcb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80'),
+('Southern Elegance','Front Porch Citrus & Eucalyptus',21.99,4.1,true,'https://images.unsplash.com/photo-1609536336627-9227b5339917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8MjY3MTE2Nzl8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60'),
+('Nudesse','OverRose',11.99,3.5,true,'https://images.unsplash.com/photo-1608977230965-6a1b92233ac6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'),
+('Harmony','Lavender,bergamot,jasmine',15.99,4.9,true,'https://images.unsplash.com/photo-1587556603704-ac65e0c1d894?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+('Girl Boss','Lavander YLANG-YLANG,Vanilla',22.99,4.5,true,'https://images.unsplash.com/photo-1589310721532-1ca233261f3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80'),
+('New York City ','Hand Poured Scented Candle',10.99,5.0,true,'https://images.unsplash.com/photo-1573744364765-f9eedccc45fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'),
+('Coziness','Cinnamon ,Vanilla , Mandarin',8.99,4.5,true,'https://images.unsplash.com/photo-1604160526773-cd287cbef8b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80');
